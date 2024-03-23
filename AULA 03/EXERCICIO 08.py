f7b87bc3f4b028f035235792e793125be132a792
Denys Gonçalves <161529989+denysg001@users.noreply.github.com>
2024-03-23 20:09:22 -0300

    atualiza exercicio 08

---
+++ AULA 03/EXERCICIO 08.py
@@ -0,0 +1,7 @@
+HORA = int(input("DIGITE A HORA: "))
+MINUTO = int(input("DIGITE OS MINUTOS: "))
+SEGUNDOS = int(input("DIGITE OS SEGUNDOS: "))
+
+TOTAL_SEGUNDOS = (HORA * 3600) + (MINUTO * 60) + SEGUNDOS
+
+print("Total Segundos:",TOTAL_SEGUNDOS )
