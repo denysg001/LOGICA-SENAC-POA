bc2d9e554498d7f6a5e82cecabd958602b2cf224
Denys Gonçalves <161529989+denysg001@users.noreply.github.com>
2024-03-23 00:24:19 -0300

    salvar

---
+++ AULA 01/AULA_01.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome:")                                     # Insere o valor digitado dentro da variavel nome
+print("Voce digitou",nome)                                           # printa o conteudo digitado na variavel nome
+
+disciplina = input("\nDigite o nome da Disciplina:")                 # Insere o valor digitado dentro da variavel disciplina
+print("Voce digitou",disciplina)                                     # printa o conteudo digitado na variavel disciplina
+
+nota = int(input("\nDigite a nota para o curso:"))                   # Insere o valor digitado dentro da variavel nota | int para numeros inteiros
+print("Voce digitou",nota)                                           # printa o conteudo digitado na variavel nota
+
+print("O seu nome é",nome,"\nO curso é:", disciplina)                # printa na tela o valor digitado na string disciplina
+print("A nota digitada foi:",nota)                                   # printa na tela o valor digitado na string nota
+print("\nO curso:",disciplina, "tem uma nota",nota)                  # printa na tela o valor digitado na string disciplina e nota
