7b4c87a6a14c97dc0caaf801ff38267a0be2c578
Denys Gonçalves <161529989+denysg001@users.noreply.github.com>
2024-03-28 19:48:37 -0300

    create

---
+++ AULA 03/Desafio - Exercícios/EXERCICIO 02.py
@@ -0,0 +1,8 @@
+# 2. Ler dois valores numéricos e escrever a soma destes. 
+
+N1 = float(input("Digite o primeiro valor: "))
+N2 = float(input("Digite o segundo valor: "))
+
+SOMA = N1 + N2
+
+print(f"O valor da soma de {N1} com {N2} é {SOMA}")
---
+++ AULA 03/Desafio - Exercícios/EXERCICIO 03.py
@@ -0,0 +1,10 @@
+# 3. Ler três valores numéricos e escrever a média aritmética. 
+
+N1 = float(input("Digite o primeiro valor: "))
+N2 = float(input("Digite o segundo valor: "))
+N3 = float(input("Digite o terceiro valor: "))
+
+
+MEDIA = (N1 + N2 + N3) / 3
+
+print(f"O valor da soma de {N1} com {N2} é {Media}")
