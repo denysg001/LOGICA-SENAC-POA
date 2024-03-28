a6b4a331b7008b1ca396ba3c7e55741b2a5a9c37
Denys Gonçalves <161529989+denysg001@users.noreply.github.com>
2024-03-28 20:34:14 -0300

    edit

--- AULA 03/Desafio - Exercícios/EXERCICIO 05.py
+++ AULA 03/Desafio - Exercícios/EXERCICIO 05.py
@@ -1,5 +1,5 @@
  #5. Escreva um algoritmo que leia dois números que deverão ser colocados, respectivamente nas variáveis vA e vB. O algoritmo deve, então, trocar os valores de vA por vB e vice-versa.
- #   Mostrar o conteúdo destas variáveis conforme a ordem de digitação antes da troca e após a troca. 
+ #    Mostrar o conteúdo destas variáveis conforme a ordem de digitação antes da troca e após a troca. 
 
 
 va=float(input("Digite Valor va:"))
