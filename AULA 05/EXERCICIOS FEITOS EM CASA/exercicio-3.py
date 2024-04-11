722686ef76e33b2e3aec0cfe20e2b6113d2f1dd8
Denys Gonçalves <161529989+denysg001@users.noreply.github.com>
2024-04-11 20:17:01 -0300

    edit

--- AULA 05/EXERCICIOS FEITOS EM CASA/exercicio-3.py
+++ AULA 05/EXERCICIOS FEITOS EM CASA/exercicio-3.py
@@ -6,7 +6,6 @@ Crie um programa que peça ao usuário dois números como entrada e imprima todo
 
 numero1 = int(input('Número Inicial: '))
 numero2 = int(input('Número Final: '))
-numero2+=1
 
 for i in range(numero1, numero2, 1):
     if i%2 != 0:
