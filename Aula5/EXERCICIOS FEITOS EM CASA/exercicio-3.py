af14bf5debc793405526d1bd1a7244db7426f292
Denys Gonçalves <161529989+denysg001@users.noreply.github.com>
2024-04-11 19:13:33 -0300

    edit

--- Aula5/EXERCICIOS FEITOS EM CASA/exercicio-3.py
+++ Aula5/EXERCICIOS FEITOS EM CASA/exercicio-3.py
@@ -11,3 +11,18 @@ numero2+=1
 for i in range(numero1, numero2, 1):
     if i%2 != 0:
         print(i)
+
+
+        '''
+3. Imprimindo Números em Intervalo Personalizado:
+Crie um programa que peça ao usuário dois números como entrada e imprima todos os números pares no intervalo especificado (inclusive).
+
+'''
+
+numero1 = int(input('Número Inicial: '))
+numero2 = int(input('Número Final: '))
+numero2+=1
+
+for i in range(numero1, numero2, 1):
+    if i%2 == 0:
+        print(i)
