196125b291ae882169260b0aa033ee6ef085e91e
Gabriel Dallemole <143417447+sam-sapi0l@users.noreply.github.com>
2024-04-11 00:53:07 -0300

    Exercicios da Aula 5 - Lógica de Programação

---
+++ Aula5/exerciciosAula5/exercicio-5.py
@@ -0,0 +1,13 @@
+'''
+
+5. Somando Elementos de uma Lista:
+Crie um programa que receba uma lista de 10  números como entrada e calcule a soma dos seus elementos.
+
+'''
+for i in range(1, 11):
+    numero = int(input('Digite um número: '))
+    lista = []
+    for i in range(1, 11):
+        lista.append(i)       
+print(lista)
+
