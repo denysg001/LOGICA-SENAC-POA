385cf7155c2b038ba12f11e0bbe0a65cea40a1b4
Denys Gonçalves <161529989+denysg001@users.noreply.github.com>
2024-04-11 20:17:10 -0300

    edit

--- AULA 06/EXERCICIOS EM AULA/EXERCICIO 01.py
+++ AULA 06/EXERCICIOS EM AULA/EXERCICIO 01.py
@@ -5,3 +5,14 @@ while(contador <= 5):
 else:
     print(f"\nA contagem terminou!")
 print(f"\nFIM!")
+
+
+contador = 0
+while(contador <= 5):
+    print(contador)
+    contador+= 1
+    if contador == 16:
+        break   #Encerra o programa sem terminar 
+else:
+    print(f"\nA contagem terminou!")
+print(f"\nFIM!")
